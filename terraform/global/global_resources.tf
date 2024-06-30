210550a05e834ee41d6e916a09e09104227597db
Prashant Warrier <pwarrier3@gmail.com>
2024-06-30 13:32:01 +0000

    create `vnet-dev` virtual network

--- terraform/global/global_resources.tf
+++ terraform/global/global_resources.tf
@@ -6,3 +6,14 @@ resource "azurerm_resource_group" "dev-rg" {
     "CreatedByTerraform" = "true"
   }
 }
+
+
+resource "azurerm_virtual_network" "vnet-dev" {
+  name = "vnet-dev"
+  address_space = [
+    "192.168.0.0/16"
+  ]
+  resource_group_name = azurerm_resource_group.dev-rg.name
+  location = azurerm_resource_group.dev-rg.location
+  tags = merge(azurerm_resource_group.dev-rg.tags, {})
+}
