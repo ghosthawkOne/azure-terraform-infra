754f44dfc39c84e9f06fa47c25b80b75244bbc2a
Prashant Warrier <61240412+ghosthawkOne@users.noreply.github.com>
2024-07-21 13:02:35 +0530

    Terraform/global/create dns record for jumpbox (#17)
    * pass `CLOUDFLARE_API_TOKEN` to terra*
    
    * make use of `cloudflare` tf provider
    
    * create DNS record using cloudflare provider
    
    * ensure DNS record is not proxied
    
    * set value of DNS record to Network interface public ip

--- terraform/global/jumpboxes.tf
+++ terraform/global/jumpboxes.tf
@@ -42,3 +42,11 @@ resource "azurerm_linux_virtual_machine" "jumpbox" {
     version   = "latest"
   }
 }
+
+resource "cloudflare_record" "jumpbox-dns" {
+  type = "A"
+  name = azurerm_linux_virtual_machine.jumpbox.name
+  zone_id = "72b99a897ca7debf9e960abcbab1c124"
+  proxied = false
+  value = azurerm_public_ip.ip-jumpbox.ip_address
+}
