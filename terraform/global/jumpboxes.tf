986e2959a2a15309a0586016529dff4abf06b9d9
Prashant Warrier <61240412+ghosthawkOne@users.noreply.github.com>
2024-09-15 19:34:18 +0530

    Teardown - Delete Public Ipv4 addr

--- terraform/global/jumpboxes.tf
+++ terraform/global/jumpboxes.tf
@@ -1,19 +1,1 @@
-resource "azurerm_public_ip" "ip-dev-box" {
-  resource_group_name = azurerm_resource_group.dev-rg.name
-  location = azurerm_resource_group.dev-rg.location
-  allocation_method = "Static"
-  name = "ip-address-dev-box"
-  sku = "Standard"
-}
 
-resource "azurerm_network_interface" "nif-dev-box" {
-  resource_group_name = azurerm_resource_group.dev-rg.name
-  location = azurerm_resource_group.dev-rg.location
-  name = "nif-dev-box"
-  ip_configuration {
-    public_ip_address_id = azurerm_public_ip.ip-dev-box.id
-    name = "nif-dev-box-public-ip-cfg"
-    private_ip_address_allocation = "Dynamic"
-    subnet_id = azurerm_subnet.subnet-pub-dev-01.id
-  }
-}
