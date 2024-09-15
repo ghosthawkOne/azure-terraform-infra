124e705b53bd4cc22b610f6e9f76afe8f243185e
Prashant Warrier <61240412+ghosthawkOne@users.noreply.github.com>
2024-09-15 19:44:48 +0530

    Delete resource group `dev-rg`

--- terraform/global/global_resources.tf
+++ terraform/global/global_resources.tf
@@ -1,9 +1,1 @@
-resource "azurerm_resource_group" "dev-rg" {
-  name     = "dev-rg"
-  location = "Central India"
-  tags = {
-    "Environment" = "dev"
-    "CreatedByTerraform" = "true"
-    "OwningProjects" = "All"
-  }
-}
+
